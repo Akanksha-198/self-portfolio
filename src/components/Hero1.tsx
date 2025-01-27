93f2f504a625f813d19547c6a021867b0d1c4c98
Akanksha-198 <kakanksha198@gmail.com>
2025-01-27 15:34:40 +0530

    a little changes

--- src/components/Hero1.tsx
+++ src/components/Hero1.tsx
@@ -37,14 +37,17 @@ export default function Hero1() {
       <div
         className="absolute inset-0 bg-gradient-to-r from-blue-800 to-purple-900 opacity-10 animate-[pulse_8s_ease-in-out_infinite]"
       ></div>
-      
-      <div className="absolute top-10 left-10 ">
+      <div className="absolute top-4 left-4 md:top-8 md:left-8">
         <Image 
-        src={logo} alt="Logo" 
-        className="h-[7rem] w-[7rem] rounded-full " />
+          src={logo} 
+          alt="Logo" 
+          className="h-12 w-12 md:h-16 md:w-16 lg:h-[7rem] lg:w-[7rem] rounded-full"
+        />
       </div>
+      
 
       <div className="relative z-10 text-center md:text-left md:w-1/2 px-6">
+      
         <h1 className="text-4xl md:text-6xl font-bold mb-4">
           {firstPart}
           <span className="text-yellow-400">{secondPart}</span>
